54cdf8e663cef1066f5fcdea6e4a985d2b6936bf
alex <west12player@gmail.com>
2021-05-07 00:53:16 +0300

    1. Курс доллара и евро по НБ РБ.

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 """Бот курсов валют"""
 import requests
 
+
+# Курс доллара и евро по НБ РБ
+url_nbrb = 'https://www.nbrb.by/api/exrates/rates?periodicity=0'
+r = requests.get(url_nbrb)
+print(r.json()[4]['Cur_Abbreviation'], '-->', r.json()[4]['Cur_OfficialRate'])
+print(r.json()[5]['Cur_Abbreviation'], '-->', r.json()[5]['Cur_OfficialRate'])
+
